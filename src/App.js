f7878381223352f22f0aec14819a72a5a27101d0
Kalys <kalys.kichinebatyrov0894@gmail.com>
2019-05-30 11:52:59 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import {Route} from 'react-router-dom';
+import SignUp from './containers/SignUp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button } from 'reactstrap';
+
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      <Nav />
+
+      <Route path="/signup" component={SignUp}/>
+
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
